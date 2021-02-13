f88bcfcc94ecc4c2c9aab989a547c3bee498e9b3
charan-ui <hurulichikkanahall.c@northeastern.edu>
2021-02-12 21:42:45 -0500

    feature of redis adding plan

---
+++ servers/redisServer.js
@@ -0,0 +1,30 @@
+/**
+ * @author Charan H M
+ * @CopyRights Rewards
+ * @Date FEB 12 , 2021
+ */
+
+var promiseFactory = require("q").promise,
+    redis = require('promise-redis')(promiseFactory);
+
+ //default port of redis
+ const REDIS_PORT = 6379;
+
+
+/**
+ * This function is for common intialisation of redis port before any operation into the db
+ */
+
+ async function redisServerInitialization() {
+      const client = redis.createClient(REDIS_PORT);
+      //on error
+      client.on("error",function (err) {
+         console.log(err);
+          return err
+      })
+    return client;
+}
+
+module.exports ={
+  redisServerInitialization
+}
